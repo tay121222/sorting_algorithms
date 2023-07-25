6f14002ac1b7519fe7ddfde63831cf81988337b5
Enoch Taylor <tay121222@gmail.com>
2023-07-25 12:00:20 +0000

    Adding all required files and solution for Task 0 - 0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swapped = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swapped;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
